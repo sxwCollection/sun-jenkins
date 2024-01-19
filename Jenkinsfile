d791cce3d501efe87b14689f550c96e3ac00afa1
sxwCollection <43781526+sxwCollection@users.noreply.github.com>
2024-01-19 14:30:08 +0100

    init

---
+++ Jenkinsfile
@@ -0,0 +1,12 @@
+pipeline {
+
+    stages {
+
+    stage ('info'){
+      steps {
+      echo "---------------------------start info ---------------"
+      sh 'mvn --version'
+      echo "---------------------------end info"}
+    }
+    }
+}
---
+++ src/main/java/com/suny/spnative01/SpNative01Application.java
@@ -0,0 +1,13 @@
+package com.suny.spnative01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpNative01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpNative01Application.class, args);
+	}
+
+}
